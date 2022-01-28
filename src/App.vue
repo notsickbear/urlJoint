8eae539c7331050056ee2f0a5f785b9a317c683d
heli <heli@123kanfang.com>
2022-01-28 17:54:25 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import ColumnList from './components/ColumnList.vue'
+import {ref} from 'vue'
+
+const list1 = ref([
+  { value: "Jesus", key: 1 },
+  { value: "Paul", key: 2 },
+  { value: "Peter", key: 3 }
+])
+
+const list2 = ref([
+      { value: "Luc", key: 5 },
+      { value: "Thomas", key: 6 },
+      { value: "John", key: 7 }
+    ]
+)
+</script>
+
+<template>
+  <div id='container'>
+    <ColumnList :list='list1'></ColumnList>
+    <ColumnList :list='list2'></ColumnList>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#container {
+  width: 80%;
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps({
+  list: Array
+})
+</script>
+
+<template>
+  <div class='list-container'>
+    <div class='list-header'>表头</div>
+    <div class='list-main-container'>
+      <div v-for='(item) in list' :key='item.key' class='list-item'>
+        <div class='list-item-key'>{{ item.key }}</div>
+        <div class='list-item-value'>{{ item.value }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src='./columnList.less'></style>
+

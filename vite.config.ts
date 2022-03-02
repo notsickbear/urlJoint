4e311c0f9b227d16fa11d156995237436f13ef95
heli <heli@123kanfang.com>
2022-03-02 13:01:39 +0800

    打包，版本 0.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({mode})  => {
+  const {
+    VITE_BASE
+  } = loadEnv(mode, process.cwd())
+  return defineConfig({
+    // 端口号(默认3000)
+    port: 3001,
+    // 是否自动打开浏览器(默认false)
+    open: true,
+    plugins: [vue()],
+    base: VITE_BASE
+  })
+}

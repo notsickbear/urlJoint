c9bb6d78f54d25cf6744b858a9791df9dcc4c247
heli <heli@123kanfang.com>
2022-03-03 11:35:03 +0800

    删除无效配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,6 @@ export default ({mode})  => {
     VITE_BASE
   } = loadEnv(mode, process.cwd())
   return defineConfig({
-    // 端口号(默认3000)
-    port: 3001,
-    // 是否自动打开浏览器(默认false)
-    open: true,
     plugins: [vue()],
     base: VITE_BASE
   })
